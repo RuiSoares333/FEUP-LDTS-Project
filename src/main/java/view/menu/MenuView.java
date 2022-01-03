ea5a370e4be8c0c3275d4dd520fd9baf4a11705f
Rui_Soares_1201355 <1201355@isep.ipp.pt>
2022-01-03 23:02:30 +0000

    Commit de Testes de MenuView

--- src/main/java/view/menu/MenuView.java
+++ src/main/java/view/menu/MenuView.java
@@ -4,109 +4,56 @@ import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
-import com.googlecode.lanterna.terminal.swing.AWTTerminalFrame;
 import model.Constants;
 import model.Menu.MenuModel;
-import view.Indicador;
+import view.IndicadorView;
 import view.View;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 public class MenuView extends View<MenuModel> {
 
-    Indicador indicador;
+    IndicadorView indicador;
     public MenuView(MenuModel model) throws IOException {
         super(model);
         initScreen();
-        indicador = new Indicador(13, 3, graphics);
+        indicador = new IndicadorView(13, 3, graphics);
     }
 
     public void draw(int position) throws IOException {
         screen.clear();
-        TextGraphics graphics = screen.newTextGraphics();
-        graphicSettings(graphics);
 
+        graphicSettings();
 
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(Constants.WIDTH, Constants.HEIGHT), ' ');
-        drawGameName(graphics);
-        drawOptions(graphics);
 
+        drawGameName();
+        drawOptions();
         indicador.draw(44, position);
+
         screen.refresh();
     }
 
-    public void graphicSettings(TextGraphics graphics){
+    public void graphicSettings(){
         graphics.enableModifiers(SGR.BOLD);
 
         graphics.setBackgroundColor(TextColor.Factory.fromString(Constants.MENU_BACKGROUND_COLOR));
         graphics.setForegroundColor(TextColor.Factory.fromString(Constants.MENU_LETTER_COLOR));
     }
 
-    public void drawOptions(TextGraphics graphics){
+    public void drawOptions(){
         graphics.putString(new TerminalPosition(48, 11), "PLAY");
         graphics.putString(new TerminalPosition(46, 13), "SETTINGS");
         graphics.putString(new TerminalPosition(45, 15), "LEADERBOARD");
         graphics.putString(new TerminalPosition(48, 19), "EXIT");
     }
 
-    public void drawGameName(TextGraphics graphics){
+    public void drawGameName(){
         for(int y=0; y<Constants.GAME_NAME.length; y++){
             graphics.putString(new TerminalPosition(27, 1+y), Constants.GAME_NAME[y]);
         }
     }
 
-    public void initScreen() throws IOException {
 
-        try {
-            screen = new TerminalScreen(getTerminal());
-        } catch (URISyntaxException | FontFormatException e) {
-            e.printStackTrace();
-        }
-        screen.setCursorPosition(null); // we don't need a cursor
-
-        screen.startScreen(); // screens must be started
-        screen.doResizeIfNecessary(); // resize screen if necessary
-
-        graphics = screen.newTextGraphics();
-    }
-
-
-    public Terminal getTerminal() throws IOException, URISyntaxException, FontFormatException {
-        URL resource = getClass().getClassLoader().getResource("Font Berzerk.ttf");
-        assert resource != null;
-        File fontFile = new File(resource.toURI());
-        Font font =  Font.createFont(Font.TRUETYPE_FONT, fontFile);
-
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        ge.registerFont(font);
-
-        DefaultTerminalFactory factory = new DefaultTerminalFactory();
-
-        Font loadedFont = font.deriveFont(Font.PLAIN, 15);
-        AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
-        factory.setTerminalEmulatorFontConfiguration(fontConfig);
-        factory.setForceAWTOverSwing(true);
-        factory.setInitialTerminalSize(new TerminalSize(Constants.WIDTH, Constants.HEIGHT));
-
-        Terminal terminal = factory.createTerminal();
-        ((AWTTerminalFrame)terminal).addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                e.getWindow().dispose();
-            }
-        });
-        return terminal;
-    }
 
 }
--- src/test/java/view/menu/MenuViewTest.java
+++ src/test/java/view/menu/MenuViewTest.java
@@ -1,40 +1,30 @@
-//package view.menu;
-//
-//import com.googlecode.lanterna.TerminalSize;
-//import com.googlecode.lanterna.graphics.TextGraphics;
-//import com.googlecode.lanterna.screen.Screen;
-//import com.googlecode.lanterna.screen.TerminalScreen;
-//import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-//import com.googlecode.lanterna.terminal.Terminal;
-//import model.Constants;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//
-//import java.io.IOException;
-//
-//public class MenuViewTest {
-//
-//    MenuView view;
-//    Screen screen;
-//    TextGraphics graphics;
-//
-//    @BeforeEach
-//    public void initView() throws IOException {
-//        TerminalSize terminalSize = new TerminalSize(Constants.WIDTH, Constants.HEIGHT);
-//        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-//        Terminal terminal = terminalFactory.createTerminal();
-//
-//        screen = new TerminalScreen(terminal);
-//        graphics = screen.newTextGraphics();
-//        view = Mockito.spy(new MenuView(screen.newTextGraphics()));
-//    }
-//
-//    @Test
-//    public void drawTest() throws IOException {
-//        view.draw(screen, 10);
-//        Mockito.verify(view, Mockito.times(1)).graphicSettings(Mockito.any());
-//        Mockito.verify(view, Mockito.times(1)).drawOptions(Mockito.any());
-//        Mockito.verify(view, Mockito.times(1)).drawGameName(Mockito.any());
-//    }
-//}
+package view.menu;
+
+import model.Menu.MenuModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import view.View;
+
+import java.io.IOException;
+
+public class MenuViewTest {
+
+    View view;
+    MenuModel model;
+
+    @BeforeEach
+    public void initView() throws IOException {
+        model = new MenuModel();
+        view = Mockito.spy(new MenuView(model));
+    }
+
+    @Test
+    public void drawTest() throws IOException {
+        view.draw(Mockito.anyInt());
+        Mockito.verify((MenuView) view, Mockito.times(1)).graphicSettings();
+        Mockito.verify((MenuView) view, Mockito.times(1)).drawGameName();
+        Mockito.verify((MenuView) view, Mockito.times(1)).drawOptions();
+
+    }
+}
