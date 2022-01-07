f814207ae816fc404d572f739fef3de633d92c34
Rui_Soares_1201355 <1201355@isep.ipp.pt>
2022-01-07 14:07:15 +0000

    Testes da Classe Ecra

---
+++ src/test/java/berzerk/model/EcraTest.java
@@ -0,0 +1,47 @@
+package berzerk.model;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.Terminal;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class EcraTest {
+
+    Screen screen;
+    TextGraphics graphics;
+    Terminal terminal;
+    Ecra ecra;
+
+
+    @BeforeEach
+    public void initEcra(){
+        screen = mock(Screen.class);
+        graphics = mock(TextGraphics.class);
+        terminal = mock(Terminal.class);
+
+        ecra = new Ecra();
+
+        ecra.setScreen(screen);
+        ecra.setGraphics(graphics);
+        ecra.setTerminal(terminal);
+    }
+
+    @Test
+    public void getScreen(){
+        Assertions.assertEquals(screen, ecra.getScreen());
+    }
+
+    @Test
+    public void getGraphics(){
+        Assertions.assertEquals(graphics, ecra.getGraphics());
+    }
+
+    @Test
+    public void getTerminal(){
+        Assertions.assertEquals(terminal, ecra.getTerminal());
+    }
+}
