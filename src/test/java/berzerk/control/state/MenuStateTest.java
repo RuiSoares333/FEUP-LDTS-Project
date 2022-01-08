34e04b9163e202ac4f0074d4696ba275e232c64d
Catarina <1201338@isep.ipp.pt>
2022-01-08 23:08:10 +0000

    Alterações ao MenuStateTest porque havia um erro

--- src/test/java/berzerk/control/state/MenuStateTest.java
+++ src/test/java/berzerk/control/state/MenuStateTest.java
@@ -1,12 +1,9 @@
 package berzerk.control.state;
 
 import berzerk.control.Command;
-import berzerk.model.Arena;
 import berzerk.model.Ecra;
 import berzerk.model.Soldado;
-import berzerk.model.entity.hero.Hero;
 import berzerk.model.menu.MenuModel;
-import berzerk.view.GameView;
 import berzerk.view.menu.MenuView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,19 +21,13 @@ public class MenuStateTest {
     FactoryState factoryState;
     Soldado soldado;
     Ecra ecra;
-    GameView gameView;
-    Arena arena;
-    Hero hero;
 
     @BeforeEach
     public void initMenuState(){
-        hero = new Hero(10,10,10,3);
-        arena = new Arena(hero, 1);
         factoryState = spy(new FactoryState());
         model = spy(new MenuModel());
         ecra = mock(Ecra.class);
         view = spy(new MenuView(model, ecra));
-        gameView = spy(new GameView(ecra, arena));
         soldado = mock(Soldado.class);
         state = new MenuState(factoryState, soldado, view);
     }
@@ -105,15 +96,6 @@ public class MenuStateTest {
         assertNull(state.run());
     }
 
-    @Test
-    public void processKeyGame() throws IOException {
-        when(gameView.getCommand()).thenAnswer(invocation -> Command.COMMAND.UP);
-        doRun(1);
-
-        when(factoryState.genGameState(mock(Soldado.class), 1)).thenAnswer(invocation -> SettingsState.class);
-
-        assertNotNull(state.run().getClass());
-    }
 
     private void doRun(int numVezes) throws IOException {
         for(int i=0; i<numVezes; i++){
