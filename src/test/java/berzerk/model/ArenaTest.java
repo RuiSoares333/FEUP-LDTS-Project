68c4c94e19ea07d34e551a927dded09c2a6d7750
Catarina <1201338@isep.ipp.pt>
2022-01-08 23:16:25 +0000

    Alterações nos testes da Arena

--- src/test/java/berzerk/model/ArenaTest.java
+++ src/test/java/berzerk/model/ArenaTest.java
@@ -1,6 +1,7 @@
 package berzerk.model;
 
 import berzerk.model.entity.Monster;
+import berzerk.model.entity.Wall;
 import berzerk.model.entity.hero.Hero;
 import berzerk.model.entity.properties.Position;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,55 +41,65 @@ public class ArenaTest {
         pass.setY(arena.getHero().getPosition().getY());
         result4 = arena.verifyMonsterCollision(pass);
 
-        assertEquals(false, result1);
-        assertEquals(false, result2);
-        assertEquals(false, result3);
-        assertEquals(true, result4);
+        assertEquals(false,result1);
+        assertEquals(false,result2);
+        assertEquals(false,result3);
+        assertEquals(true,result4);
 
     }
 
     @Test
     public void heroDieWallTest(){
-        Position positionFinal1 = new Position(10+5,10+2);
-        Position positionFinal2 = new Position(10-10,10);
-        Position positionFinal3 = new Position(10,10-8);
-        Position positionFinal4 = new Position(10,12);
-        Position positionFinal5 = new Position(10+5,10+5);
+        Wall wall1 = arena.getWalls().get(0);
+        Position wall1Position = wall1.getPosition();
+        Wall wall2 = arena.getWalls().get(1);
+        Position wall2Position = wall2.getPosition();
+        Wall wall3 = arena.getWalls().get(2);
+        Position wall3Position = wall3.getPosition();
+        Wall wall4 = arena.getWalls().get(3);
+        Position wall4Position= wall4.getPosition();
+        Position notWallPosition = new Position(10,10);
         boolean result1, result2, result3, result4, result5, result6;
 
-        arena.getHero().setPosition(positionFinal1);
+        arena.getHero().setPosition(wall1Position);
         result1 = arena.canHeroMove(arena.getHero().getPosition());
-        arena.getHero().setPosition(positionFinal5);
+        arena.getHero().setPosition(wall2Position);
         result2 = arena.verifyHeroWallCollision(arena.getHero().getPosition());
-        arena.getHero().setPosition(positionFinal2);
+        arena.getHero().setPosition(wall3Position);
         result3 = arena.canHeroMove(arena.getHero().getPosition());
-        arena.getHero().setPosition(positionFinal3);
+        arena.getHero().setPosition(notWallPosition);
         result4 = arena.canHeroMove(arena.getHero().getPosition());
-        arena.getHero().setPosition(positionFinal4);
-        result5 = arena.canHeroMove(arena.getHero().getPosition());
+        arena.getHero().setPosition(wall4Position);
+        result5 = arena.verifyHeroWallCollision(arena.getHero().getPosition());
+        arena.getHero().setPosition(notWallPosition);
         result6 = arena.verifyHeroWallCollision(arena.getHero().getPosition());
 
 
-//        assertEquals(false, result1);
-//        assertEquals(false, result2);
-//        assertEquals(false, result3);
-//        assertEquals(false, result4);
-//        assertEquals(true, result5);
-//        assertEquals(true, result6);
+        assertEquals(false, result1);
+        assertEquals(false, result2);
+        assertEquals(false, result3);
+        assertEquals(true, result4);
+        assertEquals(false, result5);
+        assertEquals(true, result6);
 
     }
 
     @Test
     public void canMonsterMoveTest(){
-        Position position1 = new Position(10+5,10+5);
-        Position position2 = new Position(11,15);
-        boolean result1, result2;
+        Wall wall1 = arena.getWalls().get(0);
+        Position position1 = wall1.getPosition();
+        Wall wall2 = arena.getWalls().get(1);
+        Position position2 = wall2.getPosition();
+        Position notWallPosition = new Position(10,10);
+        boolean result1, result2, result3;
 
         result1 = arena.canMonsterMove(position1);
         result2 = arena.canMonsterMove(position2);
+        result3 = arena.canMonsterMove(notWallPosition);
 
-//        assertEquals(false, result1);
-        assertEquals(true, result2);
+        assertEquals(false,result1);
+        assertEquals(false,result2);
+        assertEquals(true,result3);
 
 
     }
