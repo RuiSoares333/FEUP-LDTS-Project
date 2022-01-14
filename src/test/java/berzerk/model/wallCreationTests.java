f7e295d39caf1efac0911117380bdcc0e1ca78fc
Diogo <diogogomes.dev@gmail.com>
2022-01-14 15:52:01 +0000

    Teste stone position after construct up

--- src/test/java/berzerk/model/wallCreationTests.java
+++ src/test/java/berzerk/model/wallCreationTests.java
@@ -1,13 +1,31 @@
 package berzerk.model;
 
+import berzerk.model.entity.Stone;
+import berzerk.model.entity.properties.Position;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class wallCreationTests {
 
     @Test
-    public void wallPosition(){
+    public void StonePositionUp() {
+        Position position = new Position(2, 2);
+        Position newPosition;
+        Stone stone = new Stone(position, 1);
+        newPosition = new Position(2,1);
 
+        assertEquals(stone.getPosition(), newPosition);
     }
 
+    @Test
+    public void StonePositionUp() {
+        Position position = new Position(2, 2);
+        Position newPosition;
+        Stone stone = new Stone(position, 1);
+        newPosition = new Position(2,1);
+
+        assertEquals(stone.getPosition(), newPosition);
+    }
 
 }
